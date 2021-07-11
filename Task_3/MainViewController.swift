deb1c5464ff90f1d646a77a96f36082774e6f76a
kirrealdev <kirrealdev@gmail.com>
2021-07-11 15:45:59 +0300

    initial commit

---
+++ Task_3/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Task_3
+//
+//  Created by KirRealDev on 11.07.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
