76b6659419ad8dcee977467b3ae99ec4b65ff429
Aleksander <aleksander.shigin@all.me>
2021-07-12 11:29:38 +0300

    created project

---
+++ Rick&MortyApp/Rick&MortyApp/ViewControllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Rick&MortyApp
+//
+//  Created by Alexander on 12.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+        // Do any additional setup after loading the view.
+    }
+}
